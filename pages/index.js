baaaf657fb1cb536f5b49671e73346957196feeb
Timofii <mill.timofey@gmail.com>
2020-09-13 20:01:11 +0300

    testing rebuild

--- pages/index.js
+++ pages/index.js
@@ -20,10 +20,6 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h3>Learn &rarr;</h3>
